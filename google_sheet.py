3ec106e488d0fc56ec52ebc7df17a3a692206859
prsltng <84466945+prsltng@users.noreply.github.com>
2021-05-20 19:48:44 +0300

    Update google_sheet.py

--- google_sheet.py
+++ google_sheet.py
@@ -7,7 +7,9 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from multiprocessing import Pool
 chrome_options = Options()
-chrome_options.add_argument("--headless")
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 driver = webdriver.Chrome(executable_path= os.environ.get("CHROMEDRIVER_PATH") , options=chrome_options)
 
